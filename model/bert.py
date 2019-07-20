6440476660f40119465116ea74ecd787c871e7df
Ankur <ankur310794@gmail.com>
2019-07-20 16:11:32 +0530

    BertClassification class added

--- model/bert.py
+++ model/bert.py
@@ -6,6 +6,76 @@ from keras.models import Model
 from keras.engine.topology import Layer
 from bert import tokenization
 
+################# Class ########################
+
+class BertClassification(object):
+
+
+    def __init__(self, BERT_PRETRAINED_DIR):
+
+        self.bert_model = get_pretrained_model(BERT_PRETRAINED_DIR, maxlen)
+
+    def get_text_classification_model(self, LABEL_LEN, OUTPUT_ACTIVATION):
+
+        layer9 = self.bert_model.get_layer('Encoder-9-FeedForward-Norm').output
+        layer10 = self.bert_model.get_layer('Encoder-10-FeedForward-Norm').output
+        layer11 = self.bert_model.get_layer('Encoder-11-FeedForward-Norm').output
+        layer12 = self.bert_model.get_layer('Encoder-12-FeedForward-Norm').output
+
+        concatenate_layer = concatenate([layer9, layer10, layer11, layer12])
+
+        pool_output = Dense(LABEL_LEN, activation=OUTPUT_ACTIVATION,name = 'real_output')(concatenate_layer)
+
+        model  = Model(inputs=self.bert_model.input, outputs=pool_output)
+
+        return model
+
+    def get_text_classification_model_with_extra_input(self,extra_input_len, LABEL_LEN, OUTPUT_ACTIVATION):
+
+        layer9 = self.bert_model.get_layer('Encoder-9-FeedForward-Norm').output
+        layer10 = self.bert_model.get_layer('Encoder-10-FeedForward-Norm').output
+        layer11 = self.bert_model.get_layer('Encoder-11-FeedForward-Norm').output
+        layer12 = self.bert_model.get_layer('Encoder-12-FeedForward-Norm').output
+
+        concatenate_layer = concatenate([layer9, layer10, layer11, layer12])
+        concatenate_layer = NonMaskingLayer()(concatenate_layer)
+        flattened = Flatten()(concatenate_layer)
+
+        l1_input = Input(shape=(extra_input_len,), name='l1_input')
+        conc = concatenate([flattened, l1_input], axis=1)
+
+        pool_output = Dense(LABEL_LEN, activation= OUTPUT_ACTIVATION,name = 'real_output')(conc)
+
+        model  = Model(inputs=[self.bert_model.layers[0].input, self.bert_model.layers[1].input,l1_input], outputs=pool_output)
+
+        return model
+
+
+class NonMaskingLayer(Layer):
+    """
+    fix convolutional 1D can't receive masked input, detail: https://github.com/keras-team/keras/issues/4978
+    thanks for https://github.com/jacoxu
+    """
+
+    def __init__(self, **kwargs):
+        self.supports_masking = True
+        super(NonMaskingLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        pass
+
+    def compute_mask(self, input, input_mask=None):
+        # do not pass the mask to the next layers
+        return None
+
+    def call(self, x, mask=None):
+        return x
+
+    def get_output_shape_for(self, input_shape):
+        return input_shape
+
+
+################## Functions ####################
 
 def get_pretrained_model(BERT_PRETRAINED_DIR, maxlen):
 
